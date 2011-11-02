439090778e11dfdf172aea132a8a9d795063a67d
hyvi <hyvi@linux-trkm.site>
2011-11-02 18:03:36 +0800

    postmessage and window.name

---
+++ cross/js/postmessage_windowname_iframe.js
@@ -0,0 +1,45 @@
+        document.getElementById('host').innerHTML = location.host;
+
+        function send(){
+            var val = document.getElementById('data').value;
+            sendMessage(val);
+        }
+
+        (function(win, doc){
+            var ifr = win.parent;
+            
+            var cb = function(json){
+                doc.getElementById("getmsg").innerHTML = location.host+" get msg:"+json;
+            };
+            var sendMessage = function(){
+                if(win.postMessage){
+                    if (win.addEventListener) {
+                        win.addEventListener("message",function(e){
+                        cb.call(win,e.data);
+                    },false);
+                    }else if(win.attachEvent) {
+                        win.attachEvent("onmessage",function(e){
+                        cb.call(win,e.data);
+                    });
+                    }
+ 
+                    return function(data){
+                        ifr.postMessage(data,'*');
+                    };
+                }else{
+                    var hash = '';
+                    
+                    setInterval(function(){
+                        if(win.name!==hash){
+                            hash = win.name;
+                            cb.call(win,hash);
+                        }
+                    },50);
+                    return function(data){
+                        ifr.name = data;
+                    };
+                }
+            }
+            win.sendMessage = sendMessage();
+        })(window, document);
+ 
---
+++ html5/html5.md
@@ -0,0 +1,10 @@
+ Drag and Drop 
+===============
+ * site show : html5.gliese.com/html5/drop_target_for_image_using_dropzone.html
+ * code :
+    - <html5.gliese.com/html5/drop_target_for_image_using_dropzone.html> 
+    - <html5.gliese.com/html5/js/gliese.js>
+ TO DO
+-----------
+ * upload to server
+ * base64 decode
