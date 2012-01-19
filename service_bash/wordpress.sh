bb4768f4b7d879241a8df2ac650f82236289943a
hyvi <hyvi@linux-trkm.site>
2012-01-19 16:15:26 +0800

    xxxx

--- service_bash/wordpress.sh
+++ service_bash/wordpress.sh
@@ -1,1 +1,5 @@
 scp -P 5662 -r  dotcloud@wordpress.gliese.dotcloud.com:/home/dotcloud/code/ ~/wordpress
+# back up database
+mysqldump wordpress -hmysql.gliese.dotcloud.com -P 5185 -uroot -ppasswd  > wordpress.bak
+
+# TODO
