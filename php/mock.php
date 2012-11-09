67162e7197d8de21bb8569dc40ef931712b24c16
Hyvi <tanhaiwei0222@gmail.com>
2012-11-09 11:40:55 +0800

    完成php代码

--- php/mock.php
+++ php/mock.php
@@ -4,12 +4,26 @@
    $type = $_GET['type'];
    $sleep = $_GET['sleep'];
 
+   if(isset($sleep)){
+      sleep($sleep);
+   }
    if($type == 'css'){
       // 读取css  path : ./../tutorial/css/$name.css
-
+      header('Content-Type:text/css');
+      echo get_string_from_file('./../tutorial/css/'.$name.'.css');
    }else if($type == 'js'){
       // js path: ./../tutorial/js/$name.js
+      header('Content-Type:application/javascript');
+      readfile('./../tutorial/js/'.$name.'.js');
    }else {
-      $result = ['retcode'=>'000','retdes' => 'type必须为js或者css'];
+      $result = array('retcode'=>'000','retdes' => 'type必须为js或者css');
       echo json_encode($result); 
+   }
+
+   // 读取文件内容
+   function get_string_from_file($file_path){
+      $handle = fopen($file_path,'r');
+      $str = fread($handle,filesize($file_path));
+      fclose($handle);
+      return $str;
    }
---
+++ tutorial/js/order_js.js
@@ -0,0 +1,1 @@
+var injs_now = Number(new Date()); while( injs_now + (5*1000) > Number(new Date()) ) { var tmp = injs_now; }
